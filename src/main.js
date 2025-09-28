6b5497de777b2d66ee83d4049b217ceda0e5ebba
Aixright111 <1849772238@qq.com>
2025-09-28 17:06:12 +0800

    123

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from '/node_modules/vue/dist/vue.esm-bundler.js'
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 import App from './App.vue'
 
 createApp(App).mount('#app')
